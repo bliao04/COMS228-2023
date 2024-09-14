8581769acf5c625a70c3af0b9007099f87446423
bliao04 <134889456+bliao04@users.noreply.github.com>
2024-09-14 18:17:15 -0500

    hw2 upload

---
+++ hw2/PointScanner.java
@@ -0,0 +1,186 @@
+package edu.iastate.cs228.hw2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+
+/**
+ * 
+ * @author Brandon liao
+ *
+ */
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+/**
+ * 
+ * This class sorts all the points in an array of 2D points to determine a reference point whose x and y 
+ * coordinates are respectively the medians of the x and y coordinates of the original points. 
+ * 
+ * It records the employed sorting algorithm as well as the sorting time for comparison. 
+ *
+ */
+public class PointScanner  
+{
+	private Point[] points; 
+	
+	private Point medianCoordinatePoint;  // point whose x and y coordinates are respectively the medians of 
+	                                      // the x coordinates and y coordinates of those points in the array points[].
+	private Algorithm sortingAlgorithm;    
+		
+	protected long scanTime; 	       // execution time in nanoseconds. 
+	
+	/**
+	 * This constructor accepts an array of points and one of the four sorting algorithms as input. Copy 
+	 * the points into the array points[].
+	 * 
+	 * @param  pts  input array of points 
+	 * @throws IllegalArgumentException if pts == null or pts.length == 0.
+	 */
+	public PointScanner(Point[] pts, Algorithm algo) throws IllegalArgumentException
+	{
+		points = new Point[pts.length];
+		sortingAlgorithm = algo;
+		
+		for(int i = 0; i < pts.length; i++) {
+			points[i] = pts[i];
+		}
+		
+	}
+	
+	/**
+	 * This constructor reads points from a file. 
+	 * 
+	 * @param  inputFileName
+	 * @throws FileNotFoundException 
+	 * @throws InputMismatchException   if the input file contains an odd number of integers
+	 */
+	protected PointScanner(String inputFileName, Algorithm algo) throws FileNotFoundException, InputMismatchException
+	{
+		sortingAlgorithm = algo;
+		
+		int x;
+		int y;
+		File file = new File(inputFileName);
+		Scanner scnr = new Scanner(file);
+		ArrayList<Point> tempArrL = new ArrayList<Point>();
+		
+		while (scnr.hasNextInt()) {
+			x = scnr.nextInt();
+			y = scnr.nextInt();
+			tempArrL.add(new Point(x, y));
+		}	
+		
+		points = new Point[tempArrL.size()];
+		
+		for(int i = 0; i < tempArrL.size(); i++) {
+			points[i] = tempArrL.get(i);
+		}
+		
+		scnr.close();
+		
+	}
+	
+	/**
+	 * Carry out two rounds of sorting using the algorithm designated by sortingAlgorithm as follows:  
+	 *    
+	 *     a) Sort points[] by the x-coordinate to get the median x-coordinate. 
+	 *     b) Sort points[] again by the y-coordinate to get the median y-coordinate.
+	 *     c) Construct medianCoordinatePoint using the obtained median x- and y-coordinates.     
+	 *  
+	 * Based on the value of sortingAlgorithm, create an object of SelectionSorter, InsertionSorter, MergeSorter,
+	 * or QuickSorter to carry out sorting.       
+	 * @param algo
+	 * @return
+	 */
+	public void scan()
+	{
+		
+		AbstractSorter aSorter = null;
+		
+		long startTime;
+		
+		// create an object to be referenced by aSorter according to sortingAlgorithm. for each of the two 
+		if (sortingAlgorithm == Algorithm.SelectionSort) {
+			aSorter = new SelectionSorter(this.points);
+		} else if (sortingAlgorithm == Algorithm.InsertionSort) {
+			aSorter = new InsertionSorter(this.points);
+		} else if (sortingAlgorithm == Algorithm.MergeSort) {
+			aSorter = new MergeSorter(this.points);
+		} else if (sortingAlgorithm == Algorithm.QuickSort) {
+			aSorter = new QuickSorter(this.points);
+		}
+		
+		int xMedian = 0;
+		int yMedian = 0;
+		
+		startTime = System.nanoTime(); 
+		
+		for(int i = 0; i < 2; i++) {
+			
+			aSorter.setComparator(i);
+			aSorter.sort();
+			
+			if (i == 0) {
+				xMedian = aSorter.getMedian().getX();
+			} 
+			
+			if (i == 1) {
+				yMedian = aSorter.getMedian().getY();
+			}
+			medianCoordinatePoint = new Point(xMedian, yMedian);
+			
+		}
+		
+		scanTime = System.nanoTime() - startTime;
+		
+	}
+	
+	/**
+	 * Outputs performance statistics in the format: 
+	 * 
+	 * <sorting algorithm> <size>  <time>
+	 * 
+	 * For instance, 
+	 * 
+	 * selection sort   1000	  9200867
+	 * 
+	 * Use the spacing in the sample run in Section 2 of the project description. 
+	 */
+	public String stats()
+	{
+		return sortingAlgorithm + "   " + points.length + "   " + scanTime;
+	}
+	
+	
+	/**
+	 * Write MCP after a call to scan(),  in the format "MCP: (x, y)"   The x and y coordinates of the point are displayed on the same line with exactly one blank space 
+	 * in between. 
+	 */
+	@Override
+	public String toString()
+	{
+		return "MCP: (" + medianCoordinatePoint.getX() + "," + medianCoordinatePoint.getY() + ")";
+	}
+
+	/**
+	 *  
+	 * This method, called after scanning, writes point data into a file by outputFileName. The format 
+	 * of data in the file is the same as printed out from toString().  The file can help you verify 
+	 * the full correctness of a sorting result and debug the underlying algorithm. 
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public void writeMCPToFile() throws FileNotFoundException, IOException
+	{
+		BufferedWriter wrtr = new BufferedWriter(new FileWriter("Points.txt"));
+		wrtr.write(this.toString());
+		wrtr.close();
+	}	
+		
+}
